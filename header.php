b823ed7a7df764259a2c16fd77b4054a52fabe29
Manuel Urak <manuel.urak88@gmail.com>
2020-08-11 09:31:27 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Entrepeneurs For Future</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header class="main-header">
+		
+	</header>
+
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    		<span class="navbar-toggler-icon"></span>
+  		</button>
+  		<div class="collapse navbar-collapse" id="navbarNav">
+		    <ul class="navbar-nav">
+		      <li class="nav-item active">
+		        <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Sign The Petition</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Events</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Entrepeneurs</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Sustainable Business</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Stories</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Friends</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Forum</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">About Us</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Contact</a>
+		      </li>
+		    </ul>
+	  	</div>
+	</nav>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php  
+
+	include('header.php');
+
+?>
+
+	<main>
+		<section class="showcase">
+			<div class="container">
+				<a class="btn btn-primary btn-lg" href="#">Read More</a>
+			</div>
+		</section>
+	</main>
+
+<?php  
+
+	include('footer.php');
+
+?>
